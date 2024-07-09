b01801efff4a4dc1311935302d5adf3a26130b7c
Sharwin0615 <sharwinmendonca8@gmail.com>
2024-07-09 13:49:04 +0400

    db

--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:to_do_app/home_page.dart';
 
-void main() {
+Future<void> main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('mybox');
   runApp(const MyApp());
 }
 
